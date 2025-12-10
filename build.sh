f5e2a3736b3966a7d3f400d2d95cb0bc5e91e26f
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2025-12-10 10:03:13 -0300

    feat: added build

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Limpa builds anteriores
+flutter clean
+
+# Baixa dependências
+flutter pub get
+
+# Build do web com variáveis de ambiente
+flutter/bin/flutter build web --release \
+  --dart-define=FIREBASE_API_KEY="$FIREBASE_API_KEY" \
+  --dart-define=FIREBASE_AUTH_DOMAIN="$FIREBASE_AUTH_DOMAIN" \
+  --dart-define=FIREBASE_DB_URL="$FIREBASE_DB_URL" \
+  --dart-define=FIREBASE_PROJECT_ID="$FIREBASE_PROJECT_ID" \
+  --dart-define=FIREBASE_STORAGE_BUCKET="$FIREBASE_STORAGE_BUCKET" \
+  --dart-define=FIREBASE_MESSAGING_SENDER_ID="$FIREBASE_MESSAGING_SENDER_ID" \
+  --dart-define=FIREBASE_APP_ID="$FIREBASE_APP_ID"
